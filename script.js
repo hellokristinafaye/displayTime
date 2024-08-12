cd85019fb315713d4dc7c9b6729741a4076d0541
Kristina Faye <hellokristinafaye@gmail.com>
2024-08-12 11:41:13 -0700

    wrapped time function block in a setInterval() method. plugged in 1000ms for the interval, and now the page updates every second automatically to display an actual accurate time per your local time

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 
 let time = document.getElementById("current-time");
 
-let d = new Date();
-time.innerHTML = d.toLocaleTimeString();
+
+
+setInterval(() => {
+
+    let d = new Date();
+    time.innerHTML = d.toLocaleTimeString();
+}, 1000);
